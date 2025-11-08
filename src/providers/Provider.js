0d28efe1afa72ec61af1b7784f11245fa43d57c3
maudelv <mjhenriquesb29@gmail.com>
2025-11-08 17:21:55 +0100

    refactor: remove classify function and go for an only one completion method for Provider.

--- src/providers/Provider.js
+++ src/providers/Provider.js
@@ -1,42 +1,39 @@
 export default class Provider {
     /**
-     * Subclasses should override to call their underlying inference service.
-     * @param {object} options - The request options
-     * @param {string[]} options.categories - List of categories to classify into
-     * @param {string} options.destinationName - Transaction destination name
-     * @param {string} options.description - Transaction description
-     * @param {object} options.metadata - Additional metadata
-     * @param {object} options.modelOptions - Model-specific options (temperature, maxTokens, etc.)
-     */
-    async classify({categories, destinationName, description, metadata = {}, modelOptions = {}}) { // eslint-disable-line no-unused-vars
-        throw new Error(`${this.constructor.name}.classify must be implemented`);
-    }
-
-    /**
      * Get completion from the model.
      * @param {string} prompt - The prompt to send
      * @param {object} modelOptions - Model-specific options
      * @param {number} [modelOptions.temperature] - Sampling temperature (0.0-2.0)
      * @param {number} [modelOptions.max_tokens] - Maximum number of tokens to generate
+     * @returns {Promise<Object>} The completion result
      */
     async getCompletion(prompt, modelOptions = {}) { // eslint-disable-line no-unused-vars
         throw new Error(`${this.constructor.name}.getCompletion must be implemented`);
     }
 
     /**
-     * Get prompt from the model.
-     * @returns {string} prompt - The prompt to use
+     * Get classification prompt for transaction categorization.
+     * @param {object} options - The classification options
+     * @param {string[]} options.categories - List of categories to classify into
+     * @param {string} options.destinationName - Transaction destination name
+     * @param {string} options.description - Transaction description
+     * @param {object} [options.metadata={}] - Additional metadata for the transaction
+     * @returns {Promise<Object>} The classification prompt configuration to use
      */
-    async getPrompt() { // eslint-disable-line no-unused-vars
-        throw new Error(`${this.constructor.name}.getPrompt must be implemented`);
+    async getClassificationPrompt({categories, destinationName, description, metadata = {}}) { // eslint-disable-line no-unused-vars
+        throw new Error(`${this.constructor.name}.getClassificationPrompt must be implemented`);
     }
 
     /**
-     * Get expense account prompt from the model.
-     * @returns {string} prompt - The prompt to use
+     * Get expense account creation prompt for new account generation.
+     * @param {object} options - The account creation options
+     * @param {string} options.description - Transaction description
+     * @param {string} [options.destinationName] - Transaction destination/merchant name
+     * @param {object} [options.metadata={}] - Additional metadata for the transaction
+     * @returns {Promise<Object>} The expense account creation prompt configuration to use
      */
-    async getExpenseAccountPrompt() { // eslint-disable-line no-unused-vars
-        throw new Error(`${this.constructor.name}.getExpenseAccountPrompt must be implemented`);
+    async getExpenseAccountCreationPrompt({description, destinationName, metadata = {}}) { // eslint-disable-line no-unused-vars
+        throw new Error(`${this.constructor.name}.getExpenseAccountCreationPrompt must be implemented`);
     }
 
     // Expose a lightweight capability descriptor for diagnostics and UI.
